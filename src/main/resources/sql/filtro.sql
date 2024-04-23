9de7d49447094acfa0a05fa419519603d9e298b6
Kevin-Jimenez-D <122753141+Kevin-Jimenez-D@users.noreply.github.com>
2024-04-23 10:43:03 -0500

    feat: :tada: Create the proyect backend with Push-Up
    I am creating the tables and I inserted the values in a file

---
+++ src/main/resources/sql/filtro.sql
@@ -0,0 +1,119 @@
+CREATE TABLE cargos (
+    Id INT(11) NOT NULL AUTO_INCREMENT,
+    descripcion VARCHAR(50) NOT NULL,
+    sueldo_base DOUBLE NOT NULL,
+    PRIMARY KEY (Id)
+);
+
+CREATE TABLE color (
+    Id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    Descripcion VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE forma_pago (
+    Id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    descripcion VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE genero (
+    Id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    descripcion VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE insumo(
+    Id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(50) NOT NULL,
+    valor_unit DOUBLE NOT NULL,
+    stock_min DOUBLE NOT NULL,
+    stock_max DOUBLE NOT NULL,
+);
+
+CREATE TABLE pais(
+    Id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(50) NOT NULL,
+);
+
+CREATE TABLE talla(
+    Id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    descripcion VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE tipo_estado(
+    Id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    Descripcion VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE tipo_persona(
+    Id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    Nombre VARCHAR(50) NOT NULL,
+);
+
+CREATE TABLE tipo_proteccion(
+    Id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    Descripcion VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE departamento(
+    Id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(50) NOT NULL,
+    IdPaisFk INT(11) NOT NULL
+);
+
+CREATE TABLE municipio(
+    Id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(50) NOT NULL,
+    IdDepartamentoFk INT(11) NOT NULL
+);
+
+CREATE TABLE estado(
+    Id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    descripcion VARCHAR(50) NOT NULL,
+    IdTipoEstadoFk INT(11) NOT NULL
+);
+
+CREATE TABLE cliente(
+    Id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(50) NOT NULL,
+    IdCliente VARCHAR(255) NOT NULL,
+    IdTipoPersonaFk INT(11) NOT NULL,
+    fechaRegistro DATE NOT NULL,
+    IdMunicipioFk INT(11) NOT NULL
+);
+
+CREATE TABLE empleado(
+    Id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(50) NOT NULL,
+    IdCargoFk INT(11) NOT NULL,
+    fecha_ingreso DATE NOT NULL,
+    IdMunicipioFk INT(11) NOT NULL
+);
+
+CREATE TABLE venta(
+    Id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    Fecha DATE NOT NULL,
+    IdEmpleadoFk INT(11) NOT NULL,
+    IdClienteFk INT(11) NOT NULL,
+    IdFormaPagoFk INT(11) NOT NULL
+);
+
+CREATE TABLE prenda(
+    Id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    Nombre VARCHAR(50) NOT NULL,
+    ValorUnitCop DOUBLE NOT NULL,
+    ValorUnitUsd DOUBLE NOT NULL,
+    IdEstadoFk INT(11) NOT NULL,
+    IdTipoProteccion INT(11) NOT NULL,
+    IdGeneroFk INT(11) NOT NULL,
+    Codigo VARCHAR(50) NOT NULL,
+);
+
+CREATE TABLE inventario (
+    
+);
+
+CREATE TABLE insumo_prendas (
+    IdInsumoFk INT(11),
+    IdPrendaFk INT(11),
+    Cantidad INT(11)
+);
+
